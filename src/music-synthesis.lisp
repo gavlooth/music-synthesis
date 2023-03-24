8d4729db0f404d410ead475ef2fd2f40b58ae2b9
Christos Chatzifountas <heefoo@outlook.com>
2023-03-25 01:00:27 +0200

    Add guira

--- src/music-synthesis.lisp
+++ src/music-synthesis.lisp
@@ -14,8 +14,9 @@
 
 (defvar *wav-bass* (sc:buffer-read "/home/heefoo/lmms/raw/bass01.wav"))
 
-(defvar *bass-ogg* (sc:buffer-read "/home/heefoo/collider/bass01.wav"))
+(defvar *guira-file* (sc:buffer-read "/home/heefoo/lmms/raw/guira_short.wav"))
 
+; (sc:synth 'sample :buffer *guira-file*)
 (sc:defsynth drum ((freq 3000))
   (let* ((env (sc:env-gen.ar (sc:perc 0.001 0.1) :act :free))
          (sig (sc:lpf.ar (sc:white-noise.ar) (* freq env))))
@@ -31,31 +32,42 @@
 
 (defvar bass-one '('B4 'B4 'F5))
 
-(setf  *player* (sc:play (sc:synth 'sample-bass :buffer *bass-ogg* :freq 587.33 :amp 3)))
-
-(sc:free *player*)
-
 (defun default-beat (times n)
   (when (> n 0)
     (sc:at-beat 10
-                (sc:synth 'sample-bass :buffer *bass-ogg* :freq 587.33 :amp 5)
+                (sc:synth 'sample-bass :buffer *wav-bass* :freq 587.33 :amp 5)
                 (default-beat (+ 1 times) (- n 1)))))
 
 (setf *synth-definition-mode* :load)
 
-(defparameter bass-list '(:B3 :B3 :G4 :G4))
+(defparameter bass-list '(:A3 :A3 :E4 :E4))
 
-(defun base (beat)
+(defun bass (beat)
    (loop for x in  bass-list
          for y in '(0 1.5 2 3)
+         for z in '(0 1 2 3)
       do (sc:at-beat (+ beat y) (sc:synth 'sample-bass
-                                          :buffer *bass-ogg*
+                                          :buffer *wav-bass*
                                           :freq (gethash x music-notes)
-                                          :amp 1)))
-   (sc:clock-add (+ beat 4)  'base (+ beat 4)))
+                                          :amp 1))
+         (sc:at-beat (+ beat z) (sc:synth 'sample-bass
+                                          :buffer *guira-file*
+                                          :freq 440
+                                          :amp 0.4))
+         (sc:at-beat (+ beat z 0.5) (sc:synth 'sample-bass
+                                              :buffer *guira-file*
+                                              :freq 440
+                                              :amp 0.4)))
+  (sc:clock-add (+ beat 4)  'bass (+ beat 4)))
 
 (sc:CLOCK-BPM 120)
 
+; (defvar *player*)
+; (setf  *player* (sc:play (sc:synth 'sample-bass :buffer *guira-file* :freq 587.33 :amp 3)))
+;
+;  (setf  *player* (sc:play (sc:synth 'sample :buffer *guira-file*))))
+; (sc:free *player*)
+;
 
 ; (loop for x in '(:B4 :B4 :F5)
 ;        collect (gethash x music-notes))
@@ -63,7 +75,7 @@
 ; (sc:stop)
 
 
-;(base (sc:clock-quant  4))
+;(bass (sc:clock-quant  4))
 
 
 
